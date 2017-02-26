1a9e4943ce2c6279924d41cc33cf8668b60717e2
Elliot Wright <wright.elliot@gmail.com>
2017-02-26 22:25:38 +0000

    Status now accepts an entry hash.

--- pkg/cli/command-status.go
+++ pkg/cli/command-status.go
@@ -9,12 +9,19 @@ import (
 // StatusCommand creates a command to view the status of the current timer.
 func StatusCommand(gateway tracking.Gateway) console.Command {
 	var short bool
+	var hash string
 
 	configure := func(def *console.Definition) {
 		def.AddOption(
 			parameters.NewBoolValue(&short),
 			"-s, --short",
 			"Show shortened output?",
+		)
+
+		def.AddArgument(
+			parameters.NewStringValue(&hash),
+			"[HASH]",
+			"A short or long hash for an entry.",
 		)
 	}
 
@@ -24,30 +31,40 @@ func StatusCommand(gateway tracking.Gateway) console.Command {
 			return err
 		}
 
-		if !status.IsActive() {
-			output.Println("status: There is no active timer running")
+		if status.Ref() == nil || status.Ref().Entry == "" {
+			output.Println("status: No timer to check the status of")
 			return nil
 		}
 
-		entry, err := gateway.FindEntry(status.Ref().Entry)
+		if hash == "" {
+			hash = status.Ref().Entry
+		}
+
+		entry, err := gateway.FindEntry(hash)
 		if err != nil {
 			return err
 		}
 
-		// Update the duration, we're not persisting it in this command though.
-		entry.UpdateDuration()
+		isRunning := status.IsActive() && status.Ref().Entry == entry.Hash()
+
+		if isRunning {
+			// If we're viewing the status of the currently active entry, we should get make sure
+			// that it's duration is up-to-date.
+			entry.UpdateDuration()
+		}
 
 		if short {
 			output.Printf("%s on %s\n", entry.Duration(), entry.ShortHash())
 		} else {
 			dateFormat := "3:04PM (2006-01-02)"
 
+			output.Printf("Hash: %s\n", entry.Hash())
+			output.Println()
 			output.Printf("Started At: %s\n", entry.Created().Format(dateFormat))
 			output.Printf("Last Started At: %s\n", entry.Updated().Format(dateFormat))
 			output.Printf("Duration: %s\n", entry.Duration())
 			output.Printf("Note: %s\n", entry.Note())
-			output.Println()
-			output.Printf("Hash: %s\n", entry.Hash())
+			output.Printf("Running: %t\n", isRunning)
 		}
 
 		return nil
