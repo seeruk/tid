57c194828e098b40e8adec894bcbd3b407345c15
Elliot Wright <wright.elliot@gmail.com>
2017-03-09 23:42:38 +0000

    Updated version.

--- pkg/cli/application.go
+++ pkg/cli/application.go
@@ -5,14 +5,14 @@ import "github.com/eidolon/console"
 // CreateApplication builds the console application instance. Providing it with some basic
 // information like the name and version.
 func CreateApplication() *console.Application {
-	application := console.NewApplication("tid", "0.1.0-alpha.1")
+	application := console.NewApplication("tid", "0.2.0-alpha.1")
 	application.Logo = `
 ######## ### #######
    ###   ###       ##
    ###   ###  ###  ##
    ###   ###  ###  ##
    ###   ###  ######
-		`
+`
 
 	return application
 }
