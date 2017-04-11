9533dbe5cfe38efd1958e2add0453b53207ec6ff
Elliot Wright <wright.elliot@gmail.com>
2017-04-11 22:33:41 +0100

    Some kind of an experiment

--- completions/tid.bash
+++ completions/tid.bash
@@ -2,3 +2,23 @@
 # 
 # To install the completions:
 # @todo
+
+_tid() {
+    local cur prev opts
+
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts="entry report resume start status stop timesheet workspace"
+
+    if [[ ${cur} == -* || ${COMP_CWORD} -eq 1 ]] ; then
+        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+        return 0
+    fi
+
+    # case "${prev}" in
+    #
+    # esac
+}
+
+complete -F _tid tid
