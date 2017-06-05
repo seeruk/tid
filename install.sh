fbf15c9bc6d0c624d73d6a0ff9274a2492f0b493
Elliot Wright <wright.elliot@gmail.com>
2017-06-05 10:33:30 +0100

    Added notes for installation script.

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Check if ~/.tid exists, create it if not
+# Check latest version
+# Backup existing data (how do we know where the DB is? parse config for path?)
+# Move old version (move ~/.tid/bin to ~/.tid/bin-{$VERSION})
+# Download new version to ~/.tid/bin
+# Rename it
+# Make it executable (if not on Windows..?)
+# Done?
